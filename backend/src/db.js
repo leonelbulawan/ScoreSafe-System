defcc3a174eb6ed15b68bd0c5d3caccc9e3463b0
Rollin Manuel Furio <rollin.furio@sorsu.edu.ph>
2026-03-08 02:13:23 +0800

    Refactor database connection to use connection pool
    Switched from createConnection to createPool for better connection management.

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,15 +1,10 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',      // change if needed
-  database: 'scoresafe_db'
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'furiorollinmanuel',
+    database: 'scoresafe_db'
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('MySQL Connected');
-});
-
-module.exports = db;
+module.exports = db.promise();
